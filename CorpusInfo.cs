d39e877c68bce9f6be999e82edbd832c66ff886f
ST4NSB <barbulescuadrian10@gmail.com>
2019-10-30 19:01:40 +0200

    added corpus class with static met. to get genre from file name

---
+++ CorpusInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nlp
+{
+    namespace PosTagger
+    {
+        public class Corpus
+        {
+            /// <summary>
+            /// Returns the genre of the file (file name), method works only for Brown Text-Corpus (default in PosTagger)
+            /// </summary>
+            /// <param name="fileName">File name of the file corpus (ex. ca01)</param>
+            /// <returns></returns>
+            public static string GetTextGenreFromFileName(string fileName)
+            {
+                char secondLetter = fileName[1];
+                switch (secondLetter)
+                {
+                    case 'a': return "PRESS: REPORTAGE";
+                    case 'b': return "PRESS: EDITORIAL";
+                    case 'c': return "PRESS: REVIEWS";
+                    case 'd': return "RELIGION";
+                    case 'e': return "SKILL AND HOBBIES";
+                    case 'f': return "POPULAR LORE";
+                    case 'g': return "BELLES-LETTRES";
+                    case 'h': return "MISCELLANEOUS: GOVERNMENT & HOUSE ORGANS";
+                    case 'j': return "LEARNED";
+                    case 'k': return "FICTION: GENERAL";
+                    case 'l': return "FICTION: MYSTERY";
+                    case 'm': return "FICTION: SCIENCE";
+                    case 'n': return "FICTION: ADVENTURE";
+                    case 'p': return "FICTION: ROMANCE";
+                    case 'r': return "HUMOR";
+                    default: return "NO GENRE FOUND";
+                }
+            }
+        }
+    }
+}
